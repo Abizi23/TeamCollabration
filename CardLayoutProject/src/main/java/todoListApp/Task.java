3cc8827f90c352bbd60c4bd17761b8a012ad5938
natnaelmelake <natnaelmelake42@gmail.com>
2025-02-16 23:35:43 +0300

    implemented new module for todolist app

---
+++ CardLayoutProject/src/main/java/todoListApp/Task.java
@@ -0,0 +1,30 @@
+package todoListApp;
+
+public class Task {
+
+    private int taskID;
+    private String taskName;
+    private String dueDate;
+
+    public Task(int taskID,String taskName,String dueDate){
+
+        this.taskID = taskID;
+        this.taskName = taskName;
+        this.dueDate = dueDate;
+    }
+
+
+    public int getTaskID() {
+        return taskID;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+
+}
